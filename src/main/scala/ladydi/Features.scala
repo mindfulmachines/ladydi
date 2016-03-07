f1d65392236645e0b2090b4d176d735ee01330c6
Zoe Afshar <zoe.afshar@gmail.com>
2016-03-07 12:43:33 -0800

    gentrified the types of input columns for features

--- src/main/scala/ladydi/Features.scala
+++ src/main/scala/ladydi/Features.scala
@@ -9,47 +9,56 @@ import org.apache.spark.ml.{Pipeline, PipelineStage}
 class Features {
   var featureStages : List[List[PipelineStage]] = Nil
   var n: Int = 0
-  def add (input : String , stages: List[PipelineStage]): Unit = {
-    val first = stages.head
-    first.set(
-      first.getParam("inputCol"),
+  val inputColumnNames  = "inputCol" ::
+                          "inputCols" ::
+                          "featuresCol" :: Nil
+  val outputColumnNames = "outputCol" ::
+                          "topicDistributionCol" :: Nil
+
+  def inputSetter[T] (input: T, stage: PipelineStage): Unit ={
+    val inputCol = inputColumnNames.filter(stage.hasParam).head
+    stage.set(
+      stage.getParam(inputCol),
       input)
+  }
+  def outputSetter (output: String, stage: PipelineStage): Unit ={
+    val outputCol = outputColumnNames.filter(stage.hasParam).head
+    stage.set(
+      stage.getParam(outputCol),
+      output)
+  }
+
+  def add (featureName : String , stages: List[PipelineStage]): Unit = {
+    val first = stages.head
+    inputSetter(featureName, first)
 
 // chaining "stages" in an ML "PipelineStage" together
     for(s<-stages) {
       if(s != first) {
-        s.set(
-          s.getParam("inputCol"),
-          input + n)
+        inputSetter(featureName + n, s )
       }
       n += 1
-      s.set(
-        s.getParam("outputCol"),
-        input + n)
+      outputSetter(featureName + n, s)
     }
     featureStages =  stages :: featureStages
   }
 
-  def add (input : List[String] , stages: List[PipelineStage]): Unit = {
+  def add (featureNames : List[String] , stages: List[PipelineStage]): Unit = {
     val first = stages.head
-    first.set(
-      first.getParam("inputCols"),
-      input.toArray)
+    inputSetter(featureNames.toArray, first)
 
     // chaining "stages" in an ML "PipelineStage" together
     for(s<-stages) {
       if(s != first) {
-        s.set(
-          s.getParam("inputCol"),
-          input.toString + n)
+        inputSetter(featureNames.toString + n, s)
       }
       n += 1
-      s.set(
-        s.getParam("outputCol"),
-        input.toString + n)
+      outputSetter(featureNames.toString + n , s)
     }
     featureStages =  stages :: featureStages
   }
+
+
 
   def pipeline() : Pipeline = {
     val assembler = new VectorAssembler()
