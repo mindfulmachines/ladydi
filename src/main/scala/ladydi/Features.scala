e57bb4c972dd0df50bf3ede12c325f11603a3b9d
Zoe Afshar <zoe.afshar@gmail.com>
2016-03-08 17:44:10 -0800

    Added the option to encode features individually or assembled together with any kind of assembler/indexer , etc.

--- src/main/scala/ladydi/Features.scala
+++ src/main/scala/ladydi/Features.scala
@@ -28,7 +28,7 @@ class Features {
       output)
   }
 
-  def add (featureName : String , stages: List[PipelineStage]): Unit = {
+  private def chain(featureName : String , stages: List[PipelineStage]) = {
     val first = stages.head
     inputSetter(featureName, first)
 
@@ -40,39 +40,50 @@ class Features {
       n += 1
       outputSetter(featureName + n, s)
     }
-    featureStages =  stages :: featureStages
+    stages
   }
 
-  def add (featureNames : List[String] , stages: List[PipelineStage]): Unit = {
+  def add(featureName : String , stages: List[PipelineStage]): Unit = {
+    featureStages =  chain(featureName, stages) :: featureStages
+  }
+
+  private def chain (featureNames : List[String] , stages: List[PipelineStage]) = {
     val first = stages.head
     inputSetter(featureNames.toArray, first)
 
     // chaining "stages" in an ML "PipelineStage" together
-    for(s<-stages) {
-      if(s != first) {
+    for (s <- stages) {
+      if (s != first) {
         inputSetter(featureNames.toString + n, s)
       }
       n += 1
-      outputSetter(featureNames.toString + n , s)
+      outputSetter(featureNames.toString + n, s)
     }
-    featureStages =  stages :: featureStages
+    stages
   }
 
-  //todo: here we need the option to encoder features individually
-  //todo: or assembled together with VectorAssembler; as you can see
-  //todo: below, currently VectorAssembler is not a choice, it is the
-  //todo: only way.
+  def add (featureNames : List[String] , stages: List[PipelineStage]): Unit = {
+    featureStages =  chain(featureNames, stages) :: featureStages
 
-  def pipeline() : Pipeline = {
-    val assembler = new VectorAssembler()
-      .setInputCols(
+  }
+
+
+  def pipeline(output: String, stages: List[PipelineStage] = Nil) : Pipeline = {
+    if(stages.isEmpty) {
+      val noChain = featureStages.flatten
+      outputSetter(output, noChain.last)
+      new Pipeline().setStages(noChain.toArray)
+    } else {
+      val chained = chain(
         featureStages
           .map(_.last)
           .map(s => s.getOrDefault(s.getParam("outputCol")).toString)
-          .toArray
+        , stages
       )
-      .setOutputCol("features")
+      outputSetter(output, chained.last)
 
-    new Pipeline().setStages((featureStages.flatten :+ assembler).toArray)
+      new Pipeline().setStages((featureStages.flatten ::: chained).toArray)
+    }
+
   }
 }
