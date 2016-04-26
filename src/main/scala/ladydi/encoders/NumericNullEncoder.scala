6dbe1940afd3827da2136402f63e03883744a783
Zoe Afshar <zoe.afshar@gmail.com>
2016-04-26 16:15:28 -0400

    NumericNullEncoder now captures and appropriately responds to various other edge cases in weird data

--- src/main/scala/ladydi/encoders/NumericNullEncoder.scala
+++ src/main/scala/ladydi/encoders/NumericNullEncoder.scala
@@ -3,6 +3,7 @@ package ladydi.encoders
 import org.apache.spark.ml.UnaryTransformer
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.types.{DoubleType, DataType}
+import org.apache.spark.mllib.linalg._
 
 /**
   * Created by zafshar on 3/6/16.
@@ -10,23 +11,28 @@ import org.apache.spark.sql.types.{DoubleType, DataType}
 
 
 class NumericNullEncoder(override val uid: String)
-  extends UnaryTransformer[java.lang.Number, Double, NumericNullEncoder]  {
+  extends UnaryTransformer[java.lang.Number, Vector, NumericNullEncoder]  {
 Long
   def this() = this(Identifiable.randomUID("nullEncoder"))
 
 
-  protected def createTransformFunc: java.lang.Number => Double = {
+  protected def createTransformFunc: java.lang.Number => Vector = {
     NumericNullEncoder.transform
   }
 
-   protected def outputDataType: DataType = DoubleType
+   protected def outputDataType: DataType = new VectorUDT
 }
 
 object NumericNullEncoder {
 
   def load(path: String): NumericNullEncoder = new NumericNullEncoder()
 
-  def transform (x: java.lang.Number): Double = {
-    if (x == null) -1.0 ; else x.doubleValue()
+  def transform (x: java.lang.Number): Vector = {
+    if (x == null || x.doubleValue().isNaN || x.doubleValue().isInfinity) {
+      new DenseVector(Array(1.0,-1.0, -1.0)) ;
+    }
+    else {
+      new DenseVector(Array(0.0, x.doubleValue(), math.log(x.doubleValue())))
+    }
   }
 }
